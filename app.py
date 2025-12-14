53919bdfed7d4361e021572930ef2a830e12c92a
sohitpatel259 <sohitpatel359@gmail.com>
2025-12-14 15:34:57 +0530

    sat

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def health():
 
 if __name__ == '__main__':
     # Allows `flask run` or `python app.py`
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
